8e84a2e1d981734c333b060e614a1ab42f836a43
lucifer0x17 <ayush.aa347@gmail.com>
2023-09-11 23:37:15 +0530

    feat: 1st foundry project

---
+++ Resources.md
@@ -0,0 +1,2 @@
+-   Following this
+    [Course](https://www.youtube.com/playlist?list=PL4Rj_WH6yLgWe7TxankiqkrkVKXIwOP42)
---
+++ simple-storage/script/Deploy.s.sol
@@ -0,0 +1,16 @@
+// I'm a comment!
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract Deploy is Script {
+    function run() public returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
